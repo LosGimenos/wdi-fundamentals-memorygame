6fed95dd2271f20990e4f6994ca17608978ccb4c
Lorenzo Hernandez <Lorenzo Hernandez>
2016-07-15 11:55:06 -0400

    added more logic to game

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var x = cardOne;
+var y = cardTwo;
+if (x === y) {
+	alert("You found a match!")
+} else {
+	alert("Sorry try again.")
+}
