ddd42e4578f6139b8286875c5347db63a81ce13a
Lorenzo Hernandez <Lorenzo Hernandez>
2016-07-16 14:19:42 -0400

    added js logic for operable game. added css classes for flipped cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,35 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
 var grabElem = document.getElementById('game-board');
 
-
-var createBoard = function(x) {
-	for (var i = 1; i <= x; i++) {
+var createBoard = function() { 
+  for (var i = 0; i <cards.length; i++) {
 	var newCard = document.createElement('div');
-	newCard.setAttribute('class', 'card');
-	grabElem.appendChild(newCard);
+	newCard.className = 'card';
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
+  grabElem.appendChild(newCard);
+  grabElem.appendChild(newCard);
 }
-};
+}
 
+var isTwoCards = function() {
+   cardsInPlay.push(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'queen') {
+    this.className = 'flippedqueen';} else {this.className = 'flippedking'};
+  if (cardsInPlay.length === 2) {
+  isMatch(cardsInPlay);
+    cardsInPlay = [];
+}
+}  
 
-//var x = cardOne;
-//var y = cardTwo;
-//if (x === y) {
-	//alert("You found a match!")
-//} else {
-	//alert("Sorry try again.")
-//}
+var isMatch = function(cards) {
+if (cards[0] === cards[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry try again.");
+}
+}
+
+createBoard();  
