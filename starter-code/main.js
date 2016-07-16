e1241b0d9e09846c73f191ad1142c5fc6965b294
Lorenzo Hernandez <Lorenzo Hernandez>
2016-07-16 09:58:13 -0400

    added createBoard logic js and cosmetic fixes to html, css

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,10 +3,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-var x = cardOne;
-var y = cardTwo;
-if (x === y) {
-	alert("You found a match!")
-} else {
-	alert("Sorry try again.")
+var grabElem = document.getElementById('game-board');
+
+
+var createBoard = function(x) {
+	for (var i = 1; i <= x; i++) {
+	var newCard = document.createElement('div');
+	newCard.setAttribute('class', 'card');
+	grabElem.appendChild(newCard);
 }
+};
+
+
+//var x = cardOne;
+//var y = cardTwo;
+//if (x === y) {
+	//alert("You found a match!")
+//} else {
+	//alert("Sorry try again.")
+//}
